ddec4d72d0661a784e95bc4ac8f95534bcaab388
athavan.ranganathan <athavan.ranganathan@stud.hslu.ch>
2024-12-23 16:13:02 +0100

    enemy movement & enemy dies after one hit

--- Assets/Scripts/PlayerShip.cs
+++ Assets/Scripts/PlayerShip.cs
@@ -8,7 +8,6 @@ public class PlayerShip : MonoBehaviour, IDamageable
 {
     public float playerSpeed = 10f;
     public float horizontalInput;
-    public float verticalInput;
     
     public GameObject projectilePrefab;
 
@@ -21,9 +20,6 @@ public class PlayerShip : MonoBehaviour, IDamageable
     {
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector2.right * horizontalInput * playerSpeed * Time.deltaTime);
-        
-        verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.forward * verticalInput * playerSpeed * Time.deltaTime);
         
         if(Input.GetKeyDown(KeyCode.Space))
         {
